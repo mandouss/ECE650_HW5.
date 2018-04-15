226f571fb0cb783c274f5e915633ba651c9026cc
Siyue Zhou <sz132@duke.edu>
2018-04-15 14:25:36 -0400

    final

--- .sneaky_mod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vcm/650/5/sneaky_mod.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vcm/650/5/sneaky_mod.ko /home/vcm/650/5/sneaky_mod.o /home/vcm/650/5/sneaky_mod.mod.o
